29536585954a86742b23d888a2edc48e0283ecdc
germanolsd <germanolsd@gmail.com>
2025-01-26 18:52:06 -0300

    add api file with mocked loading

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+export type Device = {
+  id: string;
+  system_name: string;
+  type: string;
+  hdd_capacity: string;
+};
+
+const delay = (ms = 1100) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const fetchDevices = async (): Promise<Device[]> => {
+  await delay();
+  const response = await api.get("/devices");
+  return response.data;
+};
+
+export const fetchDeviceById = async (id: string): Promise<Device> => {
+  await delay();
+  const response = await api.get(`/devices/${id}`);
+  return response.data;
+};
+
+export default api;
