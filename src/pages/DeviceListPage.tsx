8ac1450cce3c032431fcff7137cca44a440c2e29
germanolsd <germanolsd@gmail.com>
2025-01-26 19:54:36 -0300

    refactor: replace device list rendering with DevicesList component

--- src/pages/DeviceListPage.tsx
+++ src/pages/DeviceListPage.tsx
@@ -6,12 +6,10 @@ import TitleBar from "../components/TitleBar";
 import styles from "./DeviceListPage.module.css";
 import Button from "../components/Button";
 import PlusIcon from "../components/PlusIcon";
+import DevicesList from "../components/DevicesList";
 
 const DeviceListPage = () => {
   const { data, error, isLoading } = useSWR<Device[]>("/devices", fetchDevices);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading devices</div>;
 
   return (
     <div className={styles.AppContainer}>
@@ -23,11 +21,7 @@ const DeviceListPage = () => {
             Add device
           </Button>
         </TitleBar>
-        <ul>
-          {data?.map((device) => (
-            <li key={device.id}>{device.system_name}</li>
-          ))}
-        </ul>
+        <DevicesList items={data} isLoading={isLoading} error={error} />
       </div>
     </div>
   );
