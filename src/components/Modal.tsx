2e2f6d9b9f61ef65abd4658844bcd8082b06caff
germanolsd <germanolsd@gmail.com>
2025-01-26 21:16:17 -0300

    initial modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,72 @@
+import { useRef, useEffect } from "react";
+import styles from "./Modal.module.css";
+import Button, { ButtonVisual } from "./Button";
+
+interface ModalProps {
+  open: boolean;
+  onClose?: () => void;
+  children: React.ReactNode;
+  actionButton?: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  open,
+  onClose,
+  children,
+  actionButton,
+}) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [open]);
+
+  function handleBackdropClick(
+    event: React.MouseEvent<HTMLDialogElement, MouseEvent>
+  ) {
+    if (dialogRef.current && event.target === dialogRef.current) {
+      dialogRef.current.close();
+    }
+  }
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        dialogRef.current?.close();
+        event.preventDefault();
+      }
+    };
+
+    if (open) {
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+
+    return () => {
+      if (open) {
+        document.removeEventListener("keydown", handleEscapeKey);
+      }
+    };
+  }, [open, onClose]);
+
+  return (
+    <dialog ref={dialogRef} onClose={onClose} onClick={handleBackdropClick}>
+      <div className={styles.dialogContent}>
+        {children}
+        <div className={styles.buttons}>
+          <Button
+            visual={ButtonVisual.Outline}
+            onClick={() => dialogRef.current?.close()}
+          >
+            Cancel
+          </Button>
+          {actionButton}
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
