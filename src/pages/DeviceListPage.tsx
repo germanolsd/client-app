a8b9818057a3316023475dccf9bf632d0e1b2672
germanolsd <germanolsd@gmail.com>
2025-01-28 14:42:48 -0300

    feat: integrate AddDevicesModal for device addition and update device state on creation

--- src/pages/DeviceListPage.tsx
+++ src/pages/DeviceListPage.tsx
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import { fetchDevices } from "../api";
+import { fetchDevices, createDevice, updateDevice } from "../api";
 import { type Device } from "../api";
 import TopBar from "../components/topBar/TopBar";
 import TitleBar from "../components/titleBar/TitleBar";
@@ -7,13 +7,43 @@ import styles from "./DeviceListPage.module.css";
 import Button from "../components/button/Button";
 import PlusIcon from "../components/common/PlusIcon";
 import DevicesList from "../components/devicesList/DevicesList";
-import Modal from "../components/modal/Modal";
+import AddDevicesModal from "../components/modal/AddDevicesModal";
 import { useState } from "react";
 
 const DeviceListPage = () => {
-  const { data, error, isLoading } = useSWR<Device[]>("/devices", fetchDevices);
+  const { data, error, isLoading, mutate } = useSWR<Device[]>(
+    "/devices",
+    fetchDevices
+  );
 
-  const [showModal, setShowModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  // const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleAddDevice = async (formData: Omit<Device, "id">) => {
+    try {
+      const newDevice = await createDevice(formData);
+      await mutate((devices = []) => [...devices, newDevice], {
+        revalidate: false,
+      });
+      setShowAddModal(false);
+    } catch (error) {
+      console.error("Failed to create device:", error);
+    }
+  };
+
+  // const handleUpdateDevice = async (id: string, formData: Omit<Device, "id">) => {
+  //   try {
+  //     const updatedDevice = await updateDevice(id, formData);
+  //     await mutate(
+  //       (devices = []) => devices.map(device =>
+  //         device.id === id ? updatedDevice : device
+  //       ),
+  //       { revalidate: false }
+  //     );
+  //   } catch (error) {
+  //     console.error("Failed to update device:", error);
+  //   }
+  // };
 
   return (
     <div className={styles.AppContainer}>
@@ -21,15 +51,17 @@ const DeviceListPage = () => {
 
       <div className={styles.ContentArea}>
         <TitleBar title="Devices">
-          <Button onClick={() => setShowModal(true)} icon={<PlusIcon />}>
+          <Button onClick={() => setShowAddModal(true)} icon={<PlusIcon />}>
             Add device
           </Button>
         </TitleBar>
         <DevicesList items={data} isLoading={isLoading} error={error} />
       </div>
-      <Modal open={showModal} onClose={() => setShowModal(false)}>
-        <div>Modal!</div>
-      </Modal>
+      <AddDevicesModal
+        open={showAddModal}
+        onClose={() => setShowAddModal(false)}
+        onSubmit={handleAddDevice}
+      />
     </div>
   );
 };
