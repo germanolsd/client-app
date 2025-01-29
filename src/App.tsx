4fd25e5338223d034b13c6e56ddb4a76a885cf45
germanolsd <germanolsd@gmail.com>
2025-01-29 13:13:13 -0300

    feat: remove DeviceDetailPage and simplify routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import DeviceListPage from "./pages/DeviceListPage.tsx";
-import DeviceDetailPage from "./pages/DeviceDetailPage.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/devices" element={<DeviceListPage />}>
-        <Route path=":id" element={<DeviceDetailPage />} />
-      </Route>
+      <Route path="/devices" element={<DeviceListPage />} />
       <Route path="*" element={<Navigate to="/devices" />} />
     </Routes>
   );
