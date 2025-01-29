17e71e226c67b8e1e3ad5c9e3adb77d76309464f
germanolsd <germanolsd@gmail.com>
2025-01-29 14:42:47 -0300

    added test configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "node",
-    setupFiles: "./setupTests.ts",
+    globals: true,
+    environment: "jsdom",
     include: ["src/**/*.test.tsx"],
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      provider: "v8", // Using the V8 coverage provider (default)
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{ts,tsx}"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
 });
